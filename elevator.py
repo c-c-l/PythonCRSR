4ab4788b73ff606b29ff696b22945e4c9bed8994
c-c-l <cecilia.mcszk@gmail.com>
2017-08-12 14:17:51 +0200

    add elevator conversion mini program

---
+++ elevator.py
@@ -0,0 +1,7 @@
+# PYTHON3 FILE
+# Elevator conversion program
+
+inp = input('Europe floor?')
+# NOTE : This program does not handle exceptions (ex : if NaN)
+usf = int(inp) + 1
+print('US floor', usf)
