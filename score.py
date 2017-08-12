c5b30606a488ed062161a8dd25e66f4bde654c55
c-c-l <cecilia.mcszk@gmail.com>
2017-08-12 16:45:24 +0200

    add score ex

---
+++ score.py
@@ -0,0 +1,16 @@
+# PYTHON3 FILE
+
+score = input("Enter score :")
+score = float(score)
+if score >= 0.9 :
+    print('A')
+elif score >= 0.8 :
+    print('B')
+elif score >= 0.7 :
+    print('C')
+elif >= 0.6 :
+    print('D')
+elif score < 0.6 :
+    print('F')
+else :
+    print('Enter a proper score')
