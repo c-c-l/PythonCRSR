f39bc5560a03d4fa674bffe593d9086718347e54
c-c-l <cecilia.mcszk@gmail.com>
2017-08-17 10:21:21 +0200

    add function ex

---
+++ greet.py
@@ -0,0 +1,20 @@
+# PYTHON3 FILE
+
+def greet(lang) :
+    if lang == 'es' :
+        print('Hola')
+    elif lang == 'de' :
+        print('Hallo')
+    elif lang == 'fr' :
+        print('Salut')
+    else :
+        print('Hello')
+
+print('Espagnol :')
+greet('en')
+print('Anglais :')
+greet('en')
+print('Français :')
+greet('fr')
+print('Allemand :')
+greet('de')
