c73583d86352cd1a8a068450858824f58d31d031
c-c-l <cecilia.mcszk@gmail.com>
2017-09-17 23:21:39 +0200

    add regex assignment

--- PythonWebData/regex.py
+++ PythonWebData/regex.py
@@ -1,5 +1,20 @@
 # PYTHON3 FILE
 
+# Import regex lib
 import re
 
-hand = open('')
+# Open file and declare the 2 lists
+hand = open('regex_sum_32420.txt')
+lst = list()
+nums = list()
+# Look for numbers in each lines
+for line in hand :
+    line = line.rstrip()
+    num = re.findall('[0-9]+', line)
+    # Find all data inside each num because there are some
+    # num with more than one number and convert it to an int
+    # before adding it to a new list
+    for data in num :
+        data = int(data)
+        nums.append(data)
+print(sum(nums))
